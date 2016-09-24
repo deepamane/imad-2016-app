ecca98bc1970a5d21b39a4e38284eeaea8235eb3
deepamane <deepabmane@gmail.com>
2016-09-24 15:19:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // lets move element
-var img=document.getElementByid('deepa');
+var img=document.getElementById('deepa');
 img.onclick = function(){
     img.style.marginleft='100px';
     
