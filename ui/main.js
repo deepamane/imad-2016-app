c19f9b40ebbf50c1277c58fdde4c1d2fb3c12aa6
deepamane <deepabmane@gmail.com>
2016-09-25 18:39:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,13 @@ console.log('Loaded!');
 // lets move element
 
 var img = document.getElementById('main-image');
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft + 10;
+    img.style.margineLeft= margineLeft + 'px';
+    }
+
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    var interval = setinterval(moveRight, 100);
+    
 };
